928692ace503d540fe711902c2242ac4dbf47346
Cezary <jakub.jankowski.sideapps@outlook.com>
2017-12-10 15:28:40 +0000

    fixing keyboard rover driver

--- KeyboardRoverDriver.py
+++ KeyboardRoverDriver.py
@@ -3,9 +3,9 @@ from console_controller import WsadController
 import sys
 
 if __name__ == '__main__':
-    
+
     roverCont = RoverController()
-       
+
     def go():
 	roverCont.go()
 
@@ -17,7 +17,7 @@ if __name__ == '__main__':
 
     def xBtnCallback(id):
 	roverCont.turnLeft()
-	
+
     def yBtnCallback(id):
         roverCont.goForwards()
 
@@ -26,7 +26,7 @@ if __name__ == '__main__':
 
     def stopCallback(id):
 	roverCont.stop()
-        
+
     def goCallback(id):
 	roverCont.goForwards()
 
@@ -34,23 +34,23 @@ if __name__ == '__main__':
 	xboxCont.stop()
 	roverCont.stop()
 
-    wsad = WsadController(w=(lambda: roverController.goForwards())
-   
+    wsad = WsadController(w=(lambda: roverController.goForwards()))
+
     try:
-	wsad.start()
+        wsad.start()
         print "keyboard controller running"
         while True:
             time.sleep(1)
-            
+
     #Ctrl C
     except KeyboardInterrupt:
         print "User cancelled"
-    
-    #error        
+
+    #error
     except:
         print "Unexpected error:", sys.exc_info()[0]
         raise
-        
+
     finally:
         #stop the controller
 	roverCont.stop()
