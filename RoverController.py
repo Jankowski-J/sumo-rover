e90e4dff1fc506ec3a63b856a89d9cdcc9446dcb
Jakub Jankowski <jankowski.jakub@outlook.com>
2017-12-12 18:30:11 +0100

    steering logic

--- RoverController.py
+++ RoverController.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
@@ -6,68 +7,70 @@ GPIO.setmode(GPIO.BCM)
     This class is used to control a two-motor rover, 
     which has tracks. It is assumed, that it uses a Pololu controller.
 '''
+
+
 class RoverController():
-    
-    '''
+    """
         Constructor paramaters:
         stbyPin - board number of STBY pin, which turns the Pololu controller on/off
         leftTopPin, leftBotPin - left engine inputs
         leftPowerPin - through this pin, a PWM signal for left engine is outputted
         rightTopPin, rightBotPin - right engine inputs
         rightPowerPin - through this pin, a PWM signal for right engine is outputted
-    '''
+    """
+
     def __init__(self, stbyPin=12, leftTopPin=24, leftBotPin=23,
-	leftPowerPin=18, rightTopPin=20, rightBotPin=21, rightPowerPin=16):
-	self.stbyPin = stbyPin
+                 leftPowerPin=18, rightTopPin=20, rightBotPin=21, rightPowerPin=16):
+        self.stbyPin = stbyPin
 
-	self.leftTopPin = leftTopPin
-	self.leftBotPin = leftBotPin
-	self.leftPowerPin = leftPowerPin
+        self.leftTopPin = leftTopPin
+        self.leftBotPin = leftBotPin
+        self.leftPowerPin = leftPowerPin
 
-	self.rightTopPin = rightTopPin
-	self.rightBotPin = rightBotPin
-	self.rightPowerPin = rightPowerPin
+        self.rightTopPin = rightTopPin
+        self.rightBotPin = rightBotPin
+        self.rightPowerPin = rightPowerPin
 
-	GPIO.setup(self.stbyPin, GPIO.OUT)
+        GPIO.setup(self.stbyPin, GPIO.OUT)
 
-	GPIO.setup(self.leftTopPin, GPIO.OUT)
-	GPIO.setup(self.leftBotPin, GPIO.OUT)
-	GPIO.setup(self.leftPowerPin, GPIO.OUT)
+        GPIO.setup(self.leftTopPin, GPIO.OUT)
+        GPIO.setup(self.leftBotPin, GPIO.OUT)
+        GPIO.setup(self.leftPowerPin, GPIO.OUT)
 
-	GPIO.setup(self.rightTopPin, GPIO.OUT)
-	GPIO.setup(self.rightBotPin, GPIO.OUT)
-	GPIO.setup(self.rightPowerPin, GPIO.OUT)
+        GPIO.setup(self.rightTopPin, GPIO.OUT)
+        GPIO.setup(self.rightBotPin, GPIO.OUT)
+        GPIO.setup(self.rightPowerPin, GPIO.OUT)
 
-	GPIO.output(self.stbyPin, GPIO.LOW)
+        GPIO.output(self.stbyPin, GPIO.LOW)
 
-	self.leftTopPinValue = GPIO.HIGH
-	GPIO.output(self.leftTopPin, self.leftTopPinValue)
-	self.leftBotPinValue = GPIO.HIGH
-	GPIO.output(self.leftBotPin, self.leftBotPinValue)
-	self.leftPower = GPIO.PWM(self.leftPowerPin, 50)
-	
-	self.rightTopPinValue = GPIO.HIGH
-	GPIO.output(self.rightTopPin, self.rightTopPinValue)
-	self.rightBotPinValue = GPIO.HIGH
-	GPIO.output(self.rightBotPin, self.rightBotPinValue)
-	self.rightPower = GPIO.PWM(self.rightPowerPin, 50)
+        self.leftTopPinValue = GPIO.HIGH
+        GPIO.output(self.leftTopPin, self.leftTopPinValue)
+        self.leftBotPinValue = GPIO.HIGH
+        GPIO.output(self.leftBotPin, self.leftBotPinValue)
+        self.left_engine = GPIO.PWM(self.leftPowerPin, 50)
+
+        self.rightTopPinValue = GPIO.HIGH
+        GPIO.output(self.rightTopPin, self.rightTopPinValue)
+        self.rightBotPinValue = GPIO.HIGH
+        GPIO.output(self.rightBotPin, self.rightBotPinValue)
+        self.right_engine = GPIO.PWM(self.rightPowerPin, 50)
 
     LOW_POWER_LEVEL = 25
     HIGH_POWER_LEVEL = 60
 
     def start(self):
         GPIO.output(self.stbyPin, GPIO.HIGH)
-	self.leftPower.start(RoverController.HIGH_POWER_LEVEL)
-	self.rightPower.start(RoverController.HIGH_POWER_LEVEL)
+        self.left_engine.start(RoverController.HIGH_POWER_LEVEL)
+        self.right_engine.start(RoverController.HIGH_POWER_LEVEL)
         self.goForwards()
 
     def _writeOutputs(self):
-        #print(self.leftTopPinValue, self.leftBotPinValue, self.rightTopPinValue, self.rightBotPinValue)
+        # print(self.leftTopPinValue, self.leftBotPinValue, self.rightTopPinValue, self.rightBotPinValue)
         GPIO.output(self.leftTopPin, self.leftTopPinValue)
         GPIO.output(self.leftBotPin, self.leftBotPinValue)
         GPIO.output(self.rightTopPin, self.rightTopPinValue)
         GPIO.output(self.rightBotPin, self.rightBotPinValue)
-	
+
     def _hardStop(self):
         self.leftTopPinValue = GPIO.HIGH
         self.leftBotPinValue = GPIO.HIGH
@@ -98,17 +101,17 @@ class RoverController():
         self.rightTopPinValue = GPIO.LOW
         self.rightBotPinValue = GPIO.HIGH
 
-    def _toggleLeftEnginePower(self, fullPower = True):
-	if fullPower:
-	    self.leftPower.ChangeDutyCycle(RoverController.HIGH_POWER_LEVEL)
-	else:
-	    self.leftPower.ChangeDutyCycle(RoverController.LOW_POWER_LEVEL)
+    def _toggleLeftEnginePower(self, fullPower=True):
+        if fullPower:
+            self.left_engine.ChangeDutyCycle(RoverController.HIGH_POWER_LEVEL)
+        else:
+            self.left_engine.ChangeDutyCycle(RoverController.LOW_POWER_LEVEL)
 
-    def _toggleRightEnginePower(self, fullPower = True):
-	if fullPower:
-	    self.rightPower.ChangeDutyCycle(RoverController.HIGH_POWER_LEVEL)
-	else:
-	    self.rightPower.ChangeDutyCycle(RoverController.LOW_POWER_LEVEL)
+    def _toggleRightEnginePower(self, fullPower=True):
+        if fullPower:
+            self.right_engine.ChangeDutyCycle(RoverController.HIGH_POWER_LEVEL)
+        else:
+            self.right_engine.ChangeDutyCycle(RoverController.LOW_POWER_LEVEL)
 
     def goForwards(self):
         self._spinLeftEngineClockwise()
@@ -131,12 +134,49 @@ class RoverController():
         self._toggleLeftEnginePower(True)
         self._toggleRightEnginePower(False)
         self.go()
-   
+
     def turnLeft(self):
         self._toggleLeftEnginePower(False)
         self._toggleRightEnginePower(True)
         self.go()
-	
+
     def stop(self):
         self._hardStop()
-        self._writeOutputs()	
+        self._writeOutputs()
+
+    def _rev_left_engine(self, max_power_level=100):
+        power_level = RoverController.HIGH_POWER_LEVEL * max_power_level / 100
+        self.left_engine.ChangeDutyCycle(power_level)
+
+    def _rev_right_engine(self, max_power_level=100):
+        power_level = RoverController.HIGH_POWER_LEVEL * max_power_level / 100
+        self.right_engine.ChangeDutyCycle(power_level)
+
+    @staticmethod
+    def normalize_power(power):
+        if power > 100:
+            power = 100
+        if power < -100:
+            power = -100
+
+        return power
+
+    def steer(self, power=100, left_power=100, right_power=100):
+        if power > 0:
+            self._spinLeftEngineClockwise()
+            self._spinRightEngineClockwise()
+        else:
+            self._spinLeftEngineCounterClockwise()
+            self._spinRightEngineCounterClockwise()
+
+        power = RoverController.normalize_power(power)
+        left_power = RoverController.normalize_power(left_power)
+        right_power = RoverController.normalize_power(right_power)
+
+        left_power = left_power * power / 100
+        right_power = right_power * power / 100
+
+        self._rev_left_engine(left_power)
+        self._rev_right_engine(right_power)
+
+        self._writeOutputs()
--- XboxPadRoverDriver.py
+++ XboxPadRoverDriver.py
@@ -4,80 +4,114 @@ import sys
 import time
 
 if __name__ == '__main__':
-    
-    #generic call back
+
+    # generic call back
     def controlCallBack(xboxControlId, value):
         print "Control Id = {}, Value = {}".format(xboxControlId, value)
 
-    roverCont = RoverController()
-       
-    def go():
-	roverCont.go()
+    power = 100
+    left_power = 100
+    right_power = 100
 
-    #specific callbacks for the left thumb (X & Y)
+    roverCont = RoverController()
+
+
+    def go():
+        roverCont.go()
+
+
+    # specific callbacks for the left thumb (X & Y)
     def leftThumbX(xValue):
+        is_turning_left = xValue < 0
+
+        decrease = abs(xValue)
+
+        if is_turning_left:
+            left_power = 100 - decrease
+            right_power = 100
+        else:
+            right_power = 100 - decrease
+            left_power = 100
+
+        roverCont.steer(power, left_power, right_power)
+
         print "LX {}".format(xValue)
+
+
     def leftThumbY(yValue):
+        power = yValue
+        roverCont.steer(power, left_power, right_power)
         print "LY {}".format(yValue)
 
-    #setup xbox controller, set out the deadzone and scale, also invert the Y Axis (for some reason in Pygame negative is up - wierd! 
-    xboxCont = XboxController(controlCallBack, deadzone = 30, scale = 100, invertYAxis = True, callback = go)
+
+    # setup xbox controller, set out the deadzone and scale, also invert the Y Axis (for some reason in Pygame negative is up - wierd!
+    xbox_cont = XboxController(controlCallBack, deadzone=30, scale=100, invertYAxis=True, callback=go)
+
 
     def aBtnCallback(id):
         roverCont.goBackwards()
 
+
     def bBtnCallback(id):
-	print("turning right")
+        print("turning right")
         roverCont.turnRight()
 
+
     def xBtnCallback(id):
-	print("turning left")
-	roverCont.turnLeft()
-	
+        print("turning left")
+        roverCont.turnLeft()
+
+
     def yBtnCallback(id):
         roverCont.goForwards()
 
+
     def startCallback(id):
-	roverCont.start()
+        roverCont.start()
+
 
     def stopCallback(id):
-	roverCont.stop()
-        
+        roverCont.stop()
+
+
     def goCallback(id):
-	roverCont.goForwards()
+        roverCont.goForwards()
+
 
     def exitCallback(id):
-	xboxCont.stop()
-	roverCont.stop()
-    
-    #setup the left thumb (X & Y) callbacks
-    xboxCont.setupControlCallback(xboxCont.XboxControls.LTHUMBX, leftThumbX)
-    xboxCont.setupControlCallback(xboxCont.XboxControls.LTHUMBY, leftThumbY)
-    xboxCont.setupControlCallback(xboxCont.XboxControls.A, aBtnCallback)
-    xboxCont.setupControlCallback(xboxCont.XboxControls.B, bBtnCallback)
-    xboxCont.setupControlCallback(xboxCont.XboxControls.X, xBtnCallback)
-    xboxCont.setupControlCallback(xboxCont.XboxControls.Y, yBtnCallback)
-    xboxCont.setupControlCallback(xboxCont.XboxControls.START, startCallback)
-    xboxCont.setupControlCallback(xboxCont.XboxControls.BACK, stopCallback)
-    xboxCont.setupControlCallback(xboxCont.XboxControls.XBOX, exitCallback)
-    
+        xbox_cont.stop()
+        roverCont.stop()
+
+
+    # setup the left thumb (X & Y) callbacks
+    xbox_cont.setupControlCallback(xbox_cont.XboxControls.LTHUMBX, leftThumbX)
+    xbox_cont.setupControlCallback(xbox_cont.XboxControls.LTHUMBY, leftThumbY)
+    xbox_cont.setupControlCallback(xbox_cont.XboxControls.A, aBtnCallback)
+    xbox_cont.setupControlCallback(xbox_cont.XboxControls.B, bBtnCallback)
+    xbox_cont.setupControlCallback(xbox_cont.XboxControls.X, xBtnCallback)
+    xbox_cont.setupControlCallback(xbox_cont.XboxControls.Y, yBtnCallback)
+    xbox_cont.setupControlCallback(xbox_cont.XboxControls.START, startCallback)
+    xbox_cont.setupControlCallback(xbox_cont.XboxControls.BACK, stopCallback)
+    xbox_cont.setupControlCallback(xbox_cont.XboxControls.XBOX, exitCallback)
+
+
     try:
-        #start the controller
-        xboxCont.start()
+        # start the controller
+        xbox_cont.start()
         print "xbox controller running"
         while True:
             time.sleep(1)
-            
-    #Ctrl C
+
+    # Ctrl C
     except KeyboardInterrupt:
         print "User cancelled"
-    
-    #error        
+
+    # error
     except:
         print "Unexpected error:", sys.exc_info()[0]
         raise
-        
+
     finally:
-        #stop the controller
-	xboxCont.stop()
-	roverCont.stop()
+        # stop the controller
+        xbox_cont.stop()
+        roverCont.stop()
